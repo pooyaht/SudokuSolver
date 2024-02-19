4e0ffd9dadb50c23ee5ae33c38efa5fc86ca2924
pooyaht <pooyahatami83@gmail.com>
2024-02-19 11:52:30 +0330

    Implement basic CSP algorithm

--- solver/sudoku_solver.go
+++ solver/sudoku_solver.go
@@ -1,5 +1,9 @@
 package sudokusolver
 
+import (
+	"math"
+)
+
 type SudokuSolver struct {
 	board    SudokuBoard
 	frontier []SudokuBoard
@@ -13,6 +17,27 @@ type Cell struct {
 	PossibleValues []int
 	Row            int
 	Column         int
+}
+
+func (c *Cell) removeValue(value int) {
+	for i, v := range c.PossibleValues {
+		if v == value {
+			c.PossibleValues = append(c.PossibleValues[:i], c.PossibleValues[i+1:]...)
+			break
+		}
+	}
+}
+
+func (sb SudokuBoard) getSubBoardBoundingBox(row int, column int) (int, int, int, int) {
+	n := len(sb)
+	subBoardSize := int(math.Sqrt(float64(n)))
+
+	subRowStart := (row / subBoardSize) * subBoardSize
+	subColStart := (column / subBoardSize) * subBoardSize
+	subRowEnd := subRowStart + subBoardSize - 1
+	subColEnd := subColStart + subBoardSize - 1
+
+	return subRowStart, subColStart, subRowEnd, subColEnd
 }
 
 func NewSudokuSolver(board [][]int) SudokuSolver {
@@ -51,4 +76,28 @@ func convertBoardToCells(board [][]int) SudokuBoard {
 
 func (ss SudokuSolver) GetBoard() SudokuBoard {
 	return ss.board
+}
+
+func (ss *SudokuSolver) ReducePossibleValues(cell Cell, value int) {
+	// remove from same column
+	for _, row := range ss.board {
+		if row[cell.Column].Row != cell.Row {
+			row[cell.Column].removeValue(value)
+		}
+	}
+	// remove from same row
+	for i, col := range ss.board[cell.Row] {
+		if i != cell.Column {
+			col.removeValue(value)
+		}
+	}
+	// remove from same subgrid
+	subRowStart, subColStart, subRowEnd, subColEnd := ss.board.getSubBoardBoundingBox(cell.Row, cell.Column)
+	for r := subRowStart; r <= subRowEnd; r++ {
+		for c := subColStart; c <= subColEnd; c++ {
+			if ss.board[r][c].Row != cell.Row || ss.board[r][c].Column != cell.Column {
+				ss.board[r][c].removeValue(value)
+			}
+		}
+	}
 }
