4186a96c343b80fda5b0cfd4b667ab6fcdb3b78c
pooyaht <pooyahatami83@gmail.com>
2024-02-25 18:32:42 +0330

    Add tests for parallel sudoku solver

--- solver/tests/sudoku_solver_test.go
+++ solver/tests/sudoku_solver_test.go
@@ -33,3 +33,24 @@ func TestSequentialSudokuSolver(t *testing.T) {
 		}
 	}
 }
+
+func TestParallelSudokuSolver(t *testing.T) {
+	solvableTestGrids, _ := solver.ParseSudoku(testInputPasses)
+	unsolvableTestGrids, _ := solver.ParseSudoku(testInputFails)
+	for i := range solvableTestGrids {
+		ps := solver.NewParallelSudokuSolver(solvableTestGrids[i], 4)
+		output := ps.Solve()
+		sb := solver.NewSudokuBoard(solver.ConvertBoardToCells(output))
+		if !sb.IsSolved() {
+			t.Errorf("%v is not a valid answer for %v", output, solvableTestGrids[i])
+		}
+	}
+
+	for i := range unsolvableTestGrids {
+		ps := solver.NewParallelSudokuSolver(unsolvableTestGrids[i], 4)
+		output := ps.Solve()
+		if output != nil {
+			t.Errorf("%v is solved but it is unsolvable", unsolvableTestGrids[i])
+		}
+	}
+}
